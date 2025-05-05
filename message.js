8d79a36189de5c8ffa877510b4764fbb8f310792
KRIZZY.06 <2024.kirthishs@isu.ac.in>
2025-05-05 16:51:12 +0530

    Add files via upload

---
+++ message.js
@@ -0,0 +1,23 @@
+alert("Welcome to JavaScript Workshop!!!");
+function msg(){
+    alert("Welcome to JavaScript Workshop!!!");
+}
+let name = "John Doe";
+let age = 30;
+let _x = 20;
+let $y = 10;
+let demo = `The sum of number is ${5+5}`;
+console.log(demo);
+
+let x_ = 5, y_ = "5";
+console.log(x_ === y_); 
+
+x_ = "5";
+y_ = 10;
+
+console.log(x_ === y_); 
+console.log(x_ == y_);
+console.log(x_ !== y_);
+
+let num = prompt("Enter a number");
+console.log( typeof num);
